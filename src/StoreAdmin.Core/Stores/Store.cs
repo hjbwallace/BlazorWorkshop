a76b743109577437eefc8e428b4fd08ab5944188
Harrison Wallace <hjbwallace@gmail.com>
2023-05-18 20:49:44 +0800

    Adding create and edit store pages

--- src/StoreAdmin.Core/Stores/Store.cs
+++ src/StoreAdmin.Core/Stores/Store.cs
@@ -1,11 +1,21 @@
-﻿namespace StoreAdmin.Core.Stores
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoreAdmin.Core.Stores
 {
     public class Store
     {
         public int Id { get; set; }
+
+        [Required]
         public string? Name { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string? EmailAddress { get; set; }
+
+        [Required]
         public string? Address { get; set; }
+
         public bool IsEnabled { get; set; }
     }
 }
