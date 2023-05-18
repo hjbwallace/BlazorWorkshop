c6ed00a9a64cb0bf42caf26929bc168b46fc6afa
Harrison Wallace <hjbwallace@gmail.com>
2023-05-18 21:27:27 +0800

    Adding user creation and update pages

--- src/StoreAdmin.Core/Users/User.cs
+++ src/StoreAdmin.Core/Users/User.cs
@@ -1,11 +1,20 @@
-﻿namespace StoreAdmin.Core.Users
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoreAdmin.Core.Users
 {
     public class User
     {
         public int Id { get; set; }
+
+        [Required]
         public string? Name { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string? EmailAddress { get; set; }
+
         public bool IsEnabled { get; set; }
+
         public int[] StoreIds { get; set; } = Array.Empty<int>();
     }
 }
