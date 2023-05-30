71c7a59d3f6b8bcf3963a08f5341d651ec1d0bae
Harrison Wallace <hjbwallace@gmail.com>
2023-05-30 21:44:02 +0800

    Adding store UI tests

---
+++ src/StoreAdmin.ExampleApp.Tests/Stores/StoreFormTests.cs
@@ -0,0 +1,101 @@
+﻿using StoreAdmin.Core.Stores;
+using StoreAdmin.ExampleApp.Stores;
+
+namespace StoreAdmin.ExampleApp.Tests.Stores
+{
+    public class StoreFormTests : TestContext
+    {
+        private Store? _formStore;
+
+        [Fact]
+        public void CanSubmitFormWithValidStore()
+        {
+            var store = GetValidStore();
+            var component = RenderForm();
+
+            SubmitForm(component, store);
+
+            HasSubmittedStore(store);
+            HasNoValidationErrors(component);
+        }
+
+        [Fact]
+        public void FormIsNotSubmittedWhenModelIsInvalid()
+        {
+            var component = RenderForm();
+
+            SubmitForm(component, new Store());
+
+            HasNotSubmittedStore();
+            HasValidationErrors(component,
+                "The Name field is required.",
+                "The EmailAddress field is required.",
+                "The Address field is required.");
+        }
+
+        [Fact]
+        public void FormIsNotSubmittedWhenModelHasInvalidEmail()
+        {
+            var component = RenderForm();
+
+            var store = GetValidStore();
+            store.EmailAddress = "test";
+            SubmitForm(component, store);
+
+            HasNotSubmittedStore();
+            HasValidationErrors(component, "The EmailAddress field is not a valid e-mail address.");
+        }
+
+        private static void SubmitForm(IRenderedComponent<StoreForm> component, Store store)
+        {
+            component.Find("input[id=name]").Change(store.Name);
+            component.Find("input[id=email]").Change(store.EmailAddress);
+            component.Find("input[id=address]").Change(store.Address);
+            component.Find("input[id=isEnabled]").Change(store.IsEnabled);
+            component.Find("button[type=submit]").Click();
+        }
+
+        private static void HasValidationErrors(IRenderedComponent<StoreForm> component, params string[] errors)
+        {
+            var validationErrorsElement = component!.FindAll(".validation-errors").Single();
+
+            validationErrorsElement.MarkupMatches($@"<ul class=""validation-errors"">
+    {string.Join(Environment.NewLine, errors.Select(e => $"<li class=\"validation-message\">{e}</li>"))}
+</ul>");
+        }
+
+        private static void HasNoValidationErrors(IRenderedComponent<StoreForm> component)
+        {
+            var validationErrors = component!.FindAll(".validation-errors");
+            validationErrors.Should().BeEmpty();
+        }
+
+        private static Store GetValidStore()
+        {
+            return new Store
+            {
+                Name = "Store",
+                EmailAddress = "email@test.com",
+                Address = "123 Fake Street",
+                IsEnabled = true,
+            };
+        }
+
+        private IRenderedComponent<StoreForm> RenderForm()
+        {
+            return RenderComponent<StoreForm>(parameters => parameters
+                .Add(x => x.Store, new Store())
+                .Add(x => x.OnSubmit, formStore => _formStore = formStore));
+        }
+
+        private void HasSubmittedStore(Store store)
+        {
+            _formStore.Should().BeEquivalentTo(store);
+        }
+
+        private void HasNotSubmittedStore()
+        {
+            _formStore.Should().BeNull();
+        }
+    }
+}
---
+++ src/StoreAdmin.ExampleApp.Tests/Stores/StoresPageTests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NSubstitute.ExceptionExtensions;
+using StoreAdmin.Core.Data;
+using StoreAdmin.Core.Stores;
+
+namespace StoreAdmin.ExampleApp.Tests.Stores
+{
+    public class StoresPageTests : TestContext
+    {
+        private readonly IRepository<Store> _repository;
+
+        public StoresPageTests()
+        {
+            _repository = Substitute.For<IRepository<Store>>();
+            Services.AddSingleton(_repository);
+        }
+
+        [Fact]
+        public void RendersWhenNoStoresFound()
+        {
+            HasStores();
+
+            var component = RenderComponent<ExampleApp.Stores.Stores>();
+            component.Find("#stores").MarkupMatches(@"<div id=""stores"">
+    <p>No stores found</p>
+</div>");
+        }
+
+        [Fact]
+        public void RendersWhenStoresHaveError()
+        {
+            _repository
+                .ListAsync()
+                .ThrowsAsync(new Exception("FAILED!"));
+
+            var component = RenderComponent<ExampleApp.Stores.Stores>();
+            component.Find("#stores").MarkupMatches(@"<div id=""stores"">
+    <p>Error: FAILED!</p>
+</div>");
+        }
+
+        [Fact]
+        public void RendersWhenStoresFound()
+        {
+            HasStores(new Store { Id = 1, Address = "123 Fake Street", EmailAddress = "email", IsEnabled = true, Name = "NAME" });
+
+            var component = RenderComponent<ExampleApp.Stores.Stores>();
+
+            component.Find("#stores").MarkupMatches(@"<div id=""stores"">
+    <table class=""table"">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Address</th>
+                <th>IsEnabled</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>NAME</td>
+                <td>email</td>
+                <td>123 Fake Street</td>
+                <td>True</td>
+                <td><a href=""/stores/1"" class=""btn btn-secondary"">Edit</a></td>
+            </tr>
+        </tbody>
+    </table>
+</div>");
+        }
+
+        private void HasStores(params Store[] stores)
+        {
+            _repository
+                .ListAsync()
+                .Returns(Task.FromResult(stores));
+        }
+    }
+}
