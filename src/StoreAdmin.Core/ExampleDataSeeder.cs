33acc823a5936908695be5fc3ba7e920d216f43a
Harrison Wallace <hjbwallace@gmail.com>
2023-06-22 17:04:03 +0800

    Adding mock data to the data seeding

--- src/StoreAdmin.Core/ExampleDataSeeder.cs
+++ src/StoreAdmin.Core/ExampleDataSeeder.cs
@@ -6,22 +6,56 @@ namespace StoreAdmin.Core
 {
     public static class ExampleDataSeeder
     {
+        private static readonly (string Suburb, string Postcode)[] Locations = new[]
+        {
+            ("Fremantle", "6160"),
+            ("Scarborough", "6019"),
+            ("Subiaco", "6008"),
+            ("Cottesloe", "6011"),
+            ("Joondalup", "6027"),
+            ("Victoria Park", "6100"),
+            ("Mount Lawley", "6050"),
+            ("Claremont", "6010"),
+            ("Rockingham", "6168"),
+            ("Armadale", "6112"),
+            ("South Perth", "6151"),
+            ("Leederville", "6007"),
+            ("Wembley", "6014"),
+            ("Maylands", "6051"),
+            ("Karrinyup", "6018"),
+        };
+
+        private static readonly string[] Names = new[]
+        {
+            "Joe Bloggs",
+            "John Smith",
+            "Jane Smithson",
+            "Danny Developer",
+            "Emily Engineer",
+            "Terry Tester",
+            "Sandy Supervisor",
+            "Izzy Intern",
+            "Gregory Graduate",
+            "Joey Junior",
+            "Marty Manager",
+        };
+
         public static async Task RunAsync(IRepository<Store> storeRepository, IRepository<User> userRepository)
         {
-            var stores = Enumerable.Range(1, 20).Select(x => new Store
+            var stores = Locations.Select((x, i) => new Store
             {
-                Name = $"Store {x}",
-                EmailAddress = $"store{x}@example.com",
-                Address = $"{x} Test Street, Perth, WA 6000",
+                Name = x.Suburb,
+                EmailAddress = $"{x.Suburb.Replace(" ", "").ToLower()}@example.com",
+                Address = $"{i + 1} Test Street, {x.Suburb}, WA {x.Postcode}",
                 IsEnabled = true,
-            });
+            }).ToArray();
 
-            var users = Enumerable.Range(1, 10).Select(x => new User
+            var users = Names.Select((x, i) => new User
             {
+                Name = x,
+                EmailAddress = $"{x.Replace(" ", "").ToLower()}@example.com",
+                StoreIds = Enumerable.Range(1, Locations.Length).Where(e => e % (i + 1) == 0).ToArray(),
                 IsEnabled = true,
-                Name = $"User {x}",
-                EmailAddress = $"user{x}@example.com",
-                StoreIds = Enumerable.Range(1, 20).Where(e => e % x == 0).ToArray(),
             }).ToArray();
 
             foreach (var store in stores)
